0c3e87b2129f8e876f845117ee5872bab76eb0b7
Jen Valladares <lilspikey333@yahoo.com>
2019-12-09 10:31:06 -0600

    files created

---
+++ script.js
@@ -0,0 +1,7 @@
+const question = document.querySelector('.question')
+const answer = document.querySelector('.answer')
+let url = 'https://opentdb.com/api.php?amount=10'
+
+let fetchData = () => {
+    fetch() 
+}
