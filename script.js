c72bcfb64110ff132516926bb193253dc67fe6c3
Jen Valladares <lilspikey333@yahoo.com>
2019-12-09 16:38:37 -0600

    some basic styling and API call done

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const answer = document.querySelector(".answer");
+let correctAnswer = document.querySelector(".correct-answer");
 const gameBoard = document.querySelector(".game-board");
+const searchBtn = document.querySelector("#start-button");
 let url = "https://opentdb.com/api.php?amount=10";
-
-
-
-
 
 let fetchData = () => {
   let question = document.querySelector(".question");
   fetch(url)
     .then(res => res.json())
     .then(res => {
-      
+      console.log(res);
+
       question.textContent = res.results[0].question;
+      correctAnswer.textContent = res.results[0].correct_answer;
       res.results[0].incorrect_answers.forEach(answer => {
         let answerDiv = document.createElement("div");
         answerDiv.classList.add("wrong-answer");
+        answerDiv.classList.add("answer");
         gameBoard.appendChild(answerDiv);
         answerDiv.textContent = answer;
       });
-      answer.innerHTML = res.results[0].correct_answer;
     });
 };
 
-fetchData();
+searchBtn.addEventListener("click", fetchData);
 
+/*Next step is to add event listener to the answer divs and have 
+it cycle through the results array each time it's clicked */
