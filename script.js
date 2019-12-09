bb7d7c520e5b5ca23d9c88d90507402097e66cb7
Jen Valladares <lilspikey333@yahoo.com>
2019-12-09 15:38:10 -0600

    fetch call and populating wrong answers

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const question = document.querySelector('.question')
-const answer = document.querySelector('.answer')
-let url = 'https://opentdb.com/api.php?amount=10'
+const answer = document.querySelector(".answer");
+const gameBoard = document.querySelector(".game-board");
+let url = "https://opentdb.com/api.php?amount=10";
+
+
+
+
 
 let fetchData = () => {
-    fetch() 
-}
+  let question = document.querySelector(".question");
+  fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      
+      question.textContent = res.results[0].question;
+      res.results[0].incorrect_answers.forEach(answer => {
+        let answerDiv = document.createElement("div");
+        answerDiv.classList.add("wrong-answer");
+        gameBoard.appendChild(answerDiv);
+        answerDiv.textContent = answer;
+      });
+      answer.innerHTML = res.results[0].correct_answer;
+    });
+};
+
+fetchData();
+
